ad4cf102c77f714d379ddcaf4d9eacb55e4df048
Sourav Das <iamsouravdaswade420@gmail.com>
2023-10-05 14:43:12 +0530

    find by city updated

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -6,16 +6,37 @@ class HomesController < ApplicationController
   end
 
   def search
-    @params_downcase = params[:search][:search_title].downcase
-    @found_hostel = Hostel.where("title LIKE ?", "%#{@params_downcase}%")
-    @result = 0
-    if @found_hostel and @found_hostel.size > 0
-      @result = 1
 
-      render :index
-    else
+    @params_opt = params[:opt]
+
+    puts "#{@params_opt}****************************-"
+    if @params_opt.eql?("TIT")
+
+      @params_downcase = params[:search][:search_q].downcase
+
+      @found_hostel = Hostel.where("title LIKE ?", "%#{@params_downcase}%")
       @result = 0
-      render :index
+      if @found_hostel and @found_hostel.size > 0
+        @result = 1
+
+        render :index
+      else
+        @result = 0
+        render :index
+      end
+    elsif @params_opt.eql?("CIT")
+      @params_downcase = params[:search][:search_q].downcase
+
+      @found_hostel = Hostel.where("city LIKE ?", "%#{@params_downcase}%")
+      @result = 0
+      if @found_hostel and @found_hostel.size > 0
+        @result = 1
+
+        render :index
+      else
+        @result = 0
+        render :index
+      end
     end
   end
 end
